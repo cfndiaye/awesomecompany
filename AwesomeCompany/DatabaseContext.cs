0726734a07fcefa3ae4bd9caed93d005d1aeb197
cfndiaye <cfndiaye@outlook.com>
2022-08-09 19:45:25 +0000

    initial commit

---
+++ AwesomeCompany/DatabaseContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using AwesomeCompany.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AwesomeCompany;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions options) :base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Company>(builder =>
+        {
+            builder.ToTable("Companies");
+
+            builder.
+                HasMany(company => company.Employees).
+                WithOne().
+                HasForeignKey(employe => employe.CompanyId)
+                .IsRequired();
+            builder.HasData( new Company
+            {
+                Id = 1,
+                Name = "Awesome company"
+            });
+
+        });
+
+        modelBuilder.Entity<Employee>(builder =>
+        {
+            builder.ToTable("employees");
+
+            var employees = Enumerable.Range(1, 1000).
+                Select(id => new Employee
+                {
+                    Id = id,
+                    Name = $"Employee #{id}",
+                    Salary = 100.0m,
+                    CompanyId = 1
+                });
+
+            builder.HasData(employees);
+            
+        });
+    }
+}
+
---
+++ AwesomeCompany/Program.cs
@@ -0,0 +1,98 @@
+﻿using AwesomeCompany;
+using AwesomeCompany.Entities;
+using Dapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddDbContext<DatabaseContext>(
+    o => o.UseSqlServer(builder.Configuration.GetConnectionString("Database")));
+
+var app = builder.Build();
+
+
+app.UseHttpsRedirection();
+
+
+app.MapPut("increase-salary", async (int companyId, DatabaseContext context) => {
+    var company = await context
+                            .Set<Company>()
+                            .Include(c => c.Employees)
+                            .FirstOrDefaultAsync(c => c.Id == companyId);
+    if(company is null)
+    {
+        return Results.NotFound($"The company with id:{companyId} is not found.");
+    }
+
+    //EF CORE
+    foreach (var employee in company.Employees)
+    {
+        employee.Salary *= 1.1m;
+
+    }
+
+    company.LastUpdateSalaryUtc = DateTime.UtcNow;
+
+    await context.SaveChangesAsync();
+
+    return Results.NoContent();
+
+});
+
+app.MapPut("increase-salary-sql", async (int companyId, DatabaseContext context) => {
+    var company = await context
+                            .Set<Company>()
+                            .Include(c => c.Employees)
+                            .FirstOrDefaultAsync(c => c.Id == companyId);
+    if (company is null)
+    {
+        return Results.NotFound($"The company with id:{companyId} is not found.");
+    }
+
+    await context.Database.BeginTransactionAsync();
+        //SQL
+        await context.Database.ExecuteSqlInterpolatedAsync(
+            $"UPDATE Employees SET Salary = Salary * 1.1 WHERE CompanyId = {company.Id}");
+
+        company.LastUpdateSalaryUtc = DateTime.UtcNow;
+
+        await context.SaveChangesAsync();
+
+    await context.Database.CommitTransactionAsync();
+
+    return Results.NoContent();
+
+});
+
+app.MapPut("increase-salary-sql-dapper", async (int companyId, DatabaseContext context) => {
+    var company = await context
+                            .Set<Company>()
+                            .Include(c => c.Employees)
+                            .FirstOrDefaultAsync(c => c.Id == companyId);
+    if (company is null)
+    {
+        return Results.NotFound($"The company with id:{companyId} is not found.");
+    }
+
+    var transaction = await context.Database.BeginTransactionAsync();
+    //SQL
+    await context.Database.GetDbConnection().ExecuteAsync(
+        "UPDATE Employees SET Salary = Salary * 1.1 WHERE CompanyId = @company.Id",
+        new {CompanyId = company.Id}, transaction.GetDbTransaction());
+
+    company.LastUpdateSalaryUtc = DateTime.UtcNow;
+
+    await context.SaveChangesAsync();
+
+    await context.Database.CommitTransactionAsync();
+
+    return Results.NoContent();
+
+});
+
+app.Run();
+
+
